6732d5fc59bb2b7577ec914edc9168854a25da88
Lucas5497 <lucasanselmoluizlopes@gmail.com>
2022-01-03 16:41:57 -0300

    Update imc.js

--- calculadora IMC/js/imc.js
+++ calculadora IMC/js/imc.js
@@ -1,10 +1,10 @@
 const calcular = document.getElementById('calcular');
 
 function imc(){
-    const nome= document.getElementById('nome').value;
-    const altura= document.getElementById('altura').value;
-    const peso= document.getElementById('peso').value;
-    const resultado= document.getElementById('resultado');
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
 
     if (nome !=='' && altura !=='' && peso !==''){
 
@@ -12,26 +12,23 @@ function imc(){
 
         let classificacao = '';
 
-        if (valorIMC < 19.5){
+        if(valorIMC < 19.5){
             classificacao = 'abaixo do peso.';
-        }else if (valorIMC < 25) {
+        } else if(valorIMC < 25) {
             classificacao = 'com peso ideal. Parabéns!!!';
-        }else if (valorIMC < 30){
+        } else if(valorIMC < 30){
             classificacao = 'levemente acima do peso.';
-        }else if (valorIMC < 35){
+        } else if(valorIMC < 35){
             classificacao = 'com obesidade grau I.';
-        }else if (valorIMC < 40){
-            classificacao= 'com obesidade grau II';
-        }else {
-            classificacao= 'com obesidade grau III. Cuidado!!';
+        } else if(valorIMC < 40){
+            classificacao = 'com obesidade grau II';
+        } else{
+            classificacao = 'com obesidade grau III. Cuidado!!';
         }
 
-        resultado.textContent= `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
-        
-    }else {
-        resultado.textContent= 'Preencha todos os campos!!!';
+        resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;   
+    } else{
+        resultado.textContent = 'Preencha todos os campos!!!';
     }
-
 }
-
 calcular.addEventListener('click', imc);
